acdac92e93ae8bfd5177cd35e5130e5cbd335bb0
purwaren <and.thau@gmail.com>
2020-04-29 15:53:32 +0700

    add security config

--- src/main/java/id/dailyinn/doorlock/Startup.java
+++ src/main/java/id/dailyinn/doorlock/Startup.java
@@ -5,9 +5,11 @@ import id.dailyinn.doorlock.util.DowsJnaWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
+@Component
 public class Startup {
 
     Logger logger = LoggerFactory.getLogger(getClass());
--- src/main/java/id/dailyinn/doorlock/controllers/DowsController.java
+++ src/main/java/id/dailyinn/doorlock/controllers/DowsController.java
@@ -1,8 +1,18 @@
 package id.dailyinn.doorlock.controllers;
 
+import id.dailyinn.doorlock.service.DowsJnaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DowsController {
 
+    @Autowired
+    DowsJnaService dows;
+
+    @PostMapping("/dows/connect")
+    public int connect() {
+        return dows.connect();
+    }
 }
