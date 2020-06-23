b018d4aaf3c573388183d9db0b2d94ca6856b8bd
purwaren <and.thau@gmail.com>
2020-06-23 17:12:26 +0700

    add operation

--- src/main/java/id/dailyinn/doorlock/controllers/TesaController.java
+++ src/main/java/id/dailyinn/doorlock/controllers/TesaController.java
@@ -1,9 +1,6 @@
 package id.dailyinn.doorlock.controllers;
 
-import id.dailyinn.doorlock.dto.tesa.GeneralResponse;
-import id.dailyinn.doorlock.dto.tesa.PreCheckInRequest;
-import id.dailyinn.doorlock.dto.tesa.ReadCardRequest;
-import id.dailyinn.doorlock.dto.tesa.ReadCardResponse;
+import id.dailyinn.doorlock.dto.tesa.*;
 import id.dailyinn.doorlock.service.TesaService;
 import org.apache.commons.codec.DecoderException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +21,7 @@ public class TesaController {
         return tesaService.readCard(req);
     }
 
-    @PostMapping("/tesa/pre-checkin")
+    @PostMapping("/tesa/operation")
     public GeneralResponse preCheckIn(@RequestBody PreCheckInRequest req) throws IOException, InterruptedException {
         return tesaService.preCheckIn(req);
     }
