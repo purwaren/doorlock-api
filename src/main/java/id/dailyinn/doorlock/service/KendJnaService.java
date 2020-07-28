3d06067d359dacd1c11219807adb030b626f8e64
purwaren <and.thau@gmail.com>
2020-07-28 16:48:11 +0700

    change stringbuffer to char array

--- src/main/java/id/dailyinn/doorlock/service/KendJnaService.java
+++ src/main/java/id/dailyinn/doorlock/service/KendJnaService.java
@@ -21,28 +21,28 @@ public class KendJnaService {
     }
 
     public ReadCardResponse readGuestCard() {
-        StringBuffer card = new StringBuffer();
-        StringBuffer room = new StringBuffer();
-        StringBuffer checkin = new StringBuffer();
-        StringBuffer checkout = new StringBuffer();
+        char[] card = new char[20];
+        char[] room = new char[20];
+        char[] checkin = new char[30];
+        char[] checkout = new char[30];
         int result = jnaWrapper.TP_ReadGuestCard(card, room, checkin, checkout);
         logger.info("result == {}", result);
         ReadCardResponse response = new ReadCardResponse();
         if (result > 0) {
-            response.setCardNumber(card.toString());
-            response.setRoom(room.toString());
-            response.setCheckin(checkin.toString());
-            response.setCheckout(checkout.toString());
+            response.setCardNumber(String.valueOf(card));
+            response.setRoom(String.valueOf(room));
+            response.setCheckin(String.valueOf(checkin));
+            response.setCheckout(String.valueOf(checkout));
         }
         return response;
     }
 
     public ReadCardResponse writeGuestCard(WriteCardRequest req) {
-        StringBuffer card = new StringBuffer();
+        char[] card = new char[20];
         int result = jnaWrapper.TP_MakeGuestCard(card,req.getRoom(), req.getCheckin(), req.getCheckout(), req.getFlags());
         ReadCardResponse response = new ReadCardResponse();
         if (result > 0) {
-            response.setCardNumber(card.toString());
+            response.setCardNumber(String.valueOf(card));
             response.setRoom(req.getRoom());
             response.setCheckin(req.getCheckin());
             response.setCheckout(req.getCheckout());
@@ -51,8 +51,7 @@ public class KendJnaService {
     }
 
     public void cancelCard(String card) {
-        StringBuffer cardSnr = new StringBuffer();
-        cardSnr.append(card);
+        char[] cardSnr = card.toCharArray();
         int result = jnaWrapper.TP_CancelCard(cardSnr);
         if (result > 0) {
             logger.info("card cancelled");
--- src/main/java/id/dailyinn/doorlock/util/KendJnaWrapper.java
+++ src/main/java/id/dailyinn/doorlock/util/KendJnaWrapper.java
@@ -7,8 +7,8 @@ public interface KendJnaWrapper extends Library {
     KendJnaWrapper INSTANCE = (KendJnaWrapper) Native.load("LockSDK", KendJnaWrapper.class);
 
     int TP_Configuration (int lockType);
-    int TP_MakeGuestCard (StringBuffer cardSnr, String room, String checkin, String checkout, int flags);
-    int TP_ReadGuestCard (StringBuffer cardSnr, StringBuffer room, StringBuffer checkin, StringBuffer checkout);
-    int TP_CancelCard (StringBuffer cardSnr);
-    int TP_GetCardSnr (StringBuffer cardSnr);
+    int TP_MakeGuestCard (char[] cardSnr, String room, String checkin, String checkout, int flags);
+    int TP_ReadGuestCard (char[] cardSnr, char[] room, char[] checkin, char[] checkout);
+    int TP_CancelCard (char[] cardSnr);
+    int TP_GetCardSnr (char[] cardSnr);
 }
